019d78ff78d2389cb369b3a97fc1055de2ac380c
alexenc <alexencmer@gmail.com>
2021-09-08 21:24:40 +0200

    commit inicial

---
+++ src/HomeScreen.js
@@ -0,0 +1,28 @@
+import Banner from "./Banner";
+import "./HomeScreen.css";
+import Nav from "./Nav";
+import requests from "./Requests";
+import Row from "./Row";
+
+function HomeScreen() {
+  return (
+    <div className="homeScreen">
+      <Nav />
+
+      <Banner />
+
+      <Row
+        title="Netflix originals"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+    </div>
+  );
+}
+
+export default HomeScreen;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDLvI9zivz4qoSMNgswAP4jeZ0bzaqunHM",
+  authDomain: "netflix-clone-49808.firebaseapp.com",
+  projectId: "netflix-clone-49808",
+  storageBucket: "netflix-clone-49808.appspot.com",
+  messagingSenderId: "806104987026",
+  appId: "1:806104987026:web:c1e44b0ac395dac90a7d4b",
+};
